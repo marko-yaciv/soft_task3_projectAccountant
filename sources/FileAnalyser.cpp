6cf84912eee8ad51e94c2d6ced40d5add615baf3
marko-yatsiv <2002mar4ik2002@gmail.com>
2021-01-14 00:15:51 +0200

    Created FileAnalyser class

---
+++ sources/FileAnalyser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Marko on 13.01.2021.
+//
+
+#include "FileAnalyser.h"
---
+++ sources/FileAnalyser.h
@@ -0,0 +1,16 @@
+//
+// Created by Marko on 13.01.2021.
+//
+
+#ifndef PROJECTACCOUNTANT_FILEANALYSER_H
+#define PROJECTACCOUNTANT_FILEANALYSER_H
+
+
+class FileAnalyser {
+private:
+
+public:
+};
+
+
+#endif //PROJECTACCOUNTANT_FILEANALYSER_H
